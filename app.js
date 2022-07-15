2058997ea3f21f3d33ec2bb96023181422dfb510
coffeemiller <dev2freedom@gmail.com>
2022-07-15 16:19:23 +0900

    Painting Lines

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+ctx.lineWidth = 2;
+// ctx.moveTo(0, 0);
 
-ctx.arc(250, 100, 50, 0, 2*Math.PI);
-ctx.fill();
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, Math.PI, 2*Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2*Math.PI);
-ctx.fill();
+/* https://flatuicolors.com/palette/tr */
+const colors = [
+    "#ff3838",
+    "#cd84f1",
+    "#ffcccc",
+    "#ffaf40",
+    "#fffa65",
+    "#c56cf0",
+    "#ffb8b8",
+    "#ff3838",
+    "#ff9f1a",
+    "#fff200",
+    "#32ff7e",
+    "#7efff5",
+    "#7d5fff",
+    "#4b4b4b",
+    "#3ae374",
+    "#67e6dc",
+    "#17c0eb",
+    "#7158e2",
+    "#3d3d3d"
+]
+
+function onclick(event) {
+    // console.log(event)
+    ctx.beginPath();
+    // ctx.moveTo(0, 0);
+    ctx.moveTo(400, 400);
+    // ctx.moveTo(800, 800);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onclick);
