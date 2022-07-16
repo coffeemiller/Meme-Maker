ca32edc8cbb8cad7e8642aa6785e17f15f46bb03
coffeemiller <dev2freedom@gmail.com>
2022-07-16 21:20:59 +0900

    Mouse Painging

--- app.js
+++ app.js
@@ -2,44 +2,29 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
-// ctx.moveTo(0, 0);
 
+let isPainting = false;
 
-/* https://flatuicolors.com/palette/tr */
-const colors = [
-    "#ff3838",
-    "#cd84f1",
-    "#ffcccc",
-    "#ffaf40",
-    "#fffa65",
-    "#c56cf0",
-    "#ffb8b8",
-    "#ff3838",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#7d5fff",
-    "#4b4b4b",
-    "#3ae374",
-    "#67e6dc",
-    "#17c0eb",
-    "#7158e2",
-    "#3d3d3d"
-]
-
-function onclick(event) {
-    // console.log(event)
-    ctx.beginPath();
-    // ctx.moveTo(0, 0);
-    ctx.moveTo(400, 400);
-    // ctx.moveTo(800, 800);
-    const color = colors[Math.floor(Math.random() * colors.length)];
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+function onMove(event) {
+    if(isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onclick);
+function startpainting() {
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+}
+
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startpainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting)
